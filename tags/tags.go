048ccfc958a7e92eec4e5eace4e750d10c3c4b7c
Mark Bates <mark@markbates.com>
2019-04-15 15:22:35 -0400

    add buffalo style names for tags

--- tags/tags.go
+++ tags/tags.go
@@ -4,11 +4,13 @@ import "github.com/gobuffalo/helpers/hctx"
 
 func New() hctx.Map {
 	return hctx.Map{
-		"img":    Img,
-		"imgTag": Img,
-		"css":    CSS,
-		"cssTag": CSS,
-		"js":     JS,
-		"jsTag":  JS,
+		"img":           Img,
+		"imgTag":        Img,
+		"css":           CSS,
+		"cssTag":        CSS,
+		"stylesheetTag": CSS,
+		"js":            JS,
+		"jsTag":         JS,
+		"javascriptTag": JS,
 	}
 }
